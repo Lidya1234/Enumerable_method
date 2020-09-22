e90fb631873495699044ade90f456fb6f9d152ac
Mina <cccoldatlas@gmail.com>
2020-09-22 14:30:03 +0200

    edited readme file

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Enumerable_method
+# Ruby Enumerables
+
+
+Ruby Enumerables project
+
+This project is designed for adding methods inside enumerable modules
+
+
+## Built With
+ Ruby
+ GIT
+ VSCODE
+
+## Live Demo
+Live Demo : [The-livedemo-link](https://repl.it/@MinaAnwar/MicroPettyComputationalscience#main.rb
+)
+
+<h1>Getting Started</h1>
+
+To get a local copy up and running follow these simple example steps.
+
+Prerequisites Text editor,Github profile, Git and Ruby.
+
+<h2>Authors</h2>
+
+👤 Author1
+
+- Github: [@Lidya-github ](https://github.com/Lidya1234)
+- Twitter: [@Lidya-twitter](https://twitter.com/Lidya42676629)
+- Linkedin: [Lidya-linkedin-link](https://www.linkedin.com/in/lidya-ghebreigziabher-4a94391aa/)
+
+👤 Author2
+
+- Github: [@Mina-github ](https://github.com/coldatlas)
+- Twitter: [@Mina-twitter](https://twitter.com/coldyatlas)
+- Linkedin: [Mina-linkedin-link](https://www.linkedin.com/in/coldyatlas/)
+
+## 🤝 Contributing
+
+ Contributions, issues and feature requests are welcome!
+
+Feel free to check the issues page.
+
+Show your support Give a ⭐️ if you like this project!
+
+Acknowledgments Hat tip to anyone whose code was used Inspiration etc
+
+📝 License This project is a collaboration project of microverse students. All rights are reserved for Lidya G and Mina Ibrahim.
--- enumerables.rb
+++ enumerables.rb
@@ -115,53 +115,57 @@ elsif block_given?
         total = yield(total, i)
       end
       total
+    elsif !number.nil? && symbol.nil?
+      array = to_a
+      product = number
+      (array.length -1).times do |i|
+      product = array[i + 1].send(number[0], product)
+      end
+      product
+    else
 end
 end
 end
    
   
-
+# Multiply Method
 
 def multiply_els(array)
 array.my_inject(:*) { |x , y| x * y}
 end
 
-print multiply_els([1,5,3])
 
- [1, 2, 3, 4].my_each  do|x| 
-     puts x 
-end
+
+
+# Tests
+
+
+# 1/ My Each
+ [1, 2, 3, 4].my_each  {|x| puts x }
+
+# 2/ My Each With Index
 [1, 2, 3, 4].my_each_with_index { |x, i| puts x.to_s + ':' + i.to_s if x != 3 }
 
+# 3/ My Slect
+print ([5, 6, 7, 8].my_select {|item| item!=6 })
 
-print ([5, 6, 7, 8].my_select do|item|
-  item!=6
-end
-)
-
+# 4/ My All
 puts ["cat", "dog", "human"].my_all? { |word| word.length >= 4 }
+
+# 5/ My Any
 puts ["cat", "dog", "human"].my_any? { |word| word.length >= 3 }
+
+# 6/ My None
 puts ["cat", "dog", "human"].my_none? { |word| word.length >= 6 }
 
+# 7/ My Count
 puts ([5,3,7,4,3,7].my_count(3))
 
-print ( [1,2,3,4,5,6,7].my_map do |item|
-  item >=4
-end
-)
-puts
+# 8/ My Map
+print ( [1,2,3,4,5,6,7].my_map { |item| item >=4 })
 
-puts ([1,5,5,4].my_inject do |x ,sum|
-  sum=sum+x
-  sum
-end
-)
-
-puts ([1,2,5,4].my_inject(100) do |x ,sum|
-  sum=sum+x
-  sum
-end
-)
+# 9/ Multiply
+print multiply_els([1,5,3])
 
 
 
