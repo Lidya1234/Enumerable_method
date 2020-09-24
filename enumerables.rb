50bf067860ee29c233102c9829b1bf2f1b612db8
Lidya1234 <lidya94gher@gmail.com>
2020-09-24 06:31:03 +0300

    fixed robocop error

--- enumerables.rb
+++ enumerables.rb
@@ -40,30 +40,29 @@ module Enumerable
       to_a.my_each { |i| return false if i.nil? || n == false }
 
     elsif !arg.nil? && (arg.is_a? Class)
-      to_a.my_each { |i| return false if ![i.class, i.class.superclass].include?(arg) }
+      to_a.my_each { |i| return false unless [i.class, i.class.superclass].include?(arg) }
     elsif !arg.nil? && (arg.is_a? Regexp)
-      to_a.my_each { |i| return false unless i.match(arg)}
+      to_a.my_each { |i| return false unless i.match(arg) }
     else
-      to_a.my_each { |i| return false if i != arg}
+      to_a.my_each { |i| return false if i != arg }
     end
     true
   end
 
-  
   def my_any?(arg = nil)
     if block_given?
-      to_a.my_each { |i| return true if yield(i) == true}
+      to_a.my_each { |i| return true if yield(i) == true }
       return false
     elsif arg.nil?
 
-      to_a.my_each { |i| return true unless i.nil? || i == false}
+      to_a.my_each { |i| return true unless i.nil? || i == false }
 
     elsif !arg.nil? && (arg.is_a? Class)
-      to_a.my_each { |i| return true if [i.class, i.class.superclass].include?(arg)}
+      to_a.my_each { |i| return true if [i.class, i.class.superclass].include?(arg) }
     elsif !arg.nil? && (arg.is_a? Regexp)
-      to_a.my_each { |i| return true if i.match(arg)}
+      to_a.my_each { |i| return true if i.match(arg) }
     else
-      to_a.my_each { |i| return true if i == arg}
+      to_a.my_each { |i| return true if i == arg }
     end
     false
   end
@@ -117,18 +116,14 @@ module Enumerable
       symbol = number
       number = nil
     end
-
     (raise LocalJumpError if !block_given? && number.nil? && symbol.nil?)
-
     if block_given?
       number = number.nil? ? to_a[0] : number
       to_a.my_each { |i| number = yield number, i }
 
     elsif !number.nil? && symbol.nil?
       to_a.my_each { |i| number = yield i }
-
     elsif !symbol.nil?
-
       to_a.my_each { |i| number = number.nil? ? i : number.send(symbol, i) }
 
     end
