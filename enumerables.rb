cee63d80b837d8bae47d64a2ed645cfc8b95d583
Lidya1234 <lidya94gher@gmail.com>
2020-09-24 05:55:23 +0300

    Fixed all changes required by the reviewer

--- enumerables.rb
+++ enumerables.rb
@@ -29,60 +29,83 @@ module Enumerable
     result
   end
 
-  def my_all?
-    to_a.my_each do |i|
-      return false if yield(i) == false
+  def my_all?(arg = nil)
+    if block_given?
+      to_a.my_each { |i| return false if yield(i) == false}
 
       return true
-    end
-  end
+    elsif arg.nil?
+      to_a.my_each { |i| return false if i.nil? || n == false}
 
-  def my_any?
-    return to_enum(:my_any?) unless block_given?
-
-    i = 0
-    ans = false
-    to_a.length.times do
-      ans = yield to_a[i]
-
-      break if ans == true
-
-      i += 1
-    end
-    ans
-  end
-
-  def my_none?
-    return to_enum(:my_none?) unless block_given?
-
-    i = 0
-    ans = false
-    to_a.length.times do
-      ans = yield to_a[i]
-
-      break if ans == true
-
-      i += 1
-    end
-    !ans
-  end
-
-  def my_count(number = nil)
-    count = 0
-    if number.nil?
-      count = to_a.length
+    elsif !arg.nil? && (arg.is_a? Class)
+      to_a.my_each { |i| return false if i.class != arg && i.class.superclass != arg}
+    elsif !arg.nil? && (arg.is_a? Regexp)
+      to_a.my_each { |i| return false unless i.match(arg)}
     else
-
-      my_each { |i| count += 1 if number == i }
-
-      print count
+      to_a.my_each { |i| return false if i != arg}
     end
+    true
+  end
+
+  def my_any?(arg = nil)
+    if block_given?
+      to_a.my_each { |i| return true if yield(i) == true}
+      return false
+    elsif arg.nil?
+
+      to_a.my_each { |i| return true unless i.nil? || i == false}
+
+    elsif !arg.nil? && (arg.is_a? Class)
+      to_a.my_each { |i| return true if i.class == arg || i.class.superclass == arg}
+    elsif !arg.nil? && (arg.is_a? Regexp)
+      to_a.my_each { |i| return true if i.match(arg)}
+    else
+      to_a.my_each { |i| return true if i = arg}
+    end
+    false
+  end
+
+  def my_none?(arg = nil)
+    if block_given?
+      to_a.my_each { |i| return false if yield(i) == true}
+      return true
+    elsif arg.nil?
+      to_a.my_each { |i| return false if i }
+
+    elsif !arg.nil? && (arg.is_a? Class)
+      to_a.my_each { |i| return false if i.class == arg || i.class.superclass == arg}
+
+    elsif !arg.nil? && (arg.is_a? Regexp)
+      to_a.my_each { |i| return false if i.match(arg)}
+    else
+      to_a.my_each { |i| return false if i == arg}
+    end
+    true
+  end
+
+  def my_count(arg = nil)
+    count = 0
+    if block_given?
+      to_a.my_each { |i| count += 1 if yield i}
+    elsif !arg.nil?
+      to_a.my_each { |i| count += 1 if i == arg }
+    else
+      to_a.my_each { |i| count += 1 if i}
+    end
+    return count
   end
 
   def my_map(proc = nil)
+    return to_enum(:my_map) unless block_given? || !proc.nil?
+
     result = []
 
-    to_a.my_each { |item| result << item if proc.call(item) }
+    if block_given?
+      to_a.my_each { |item| result << yield(item) }
+    else
+      to_a.my_each { |item| result << item if proc.call(item) }
+
+    end
     result
   end
   # rubocop:disable Metrics/CyclomaticComplexity
@@ -120,34 +143,3 @@ end
 def multiply_els(array)
   array.my_inject(:*) { |x, y| x * y }
 end
-
-# Tests
-
-puts '--My Each'
-[1, 2, 3, 4].my_each { |x| puts x }
-
-puts 'My Each With Index'
-[1, 2, 3, 4].my_each_with_index { |x, i| puts x.to_s + ':' + i.to_s if x != 3 }
-
-puts 'My Select'
-print([5, 6, 7, 8].my_select { |item| item != 6 })
-puts
-puts 'My All'
-puts(%w[cat dog human].my_all? { |word| word.length >= 4 })
-
-puts 'My Any'
-puts(%w[cat dog human].my_any? { |word| word.length >= 3 })
-
-puts 'My none'
-puts(%w[cat dog human].my_none? { |word| word.length >= 6 })
-
-puts 'My count'
-puts([5, 3, 7, 4, 3, 7].my_count(3))
-
-puts 'My map'
-
-print([1, 2, 3, 8, 9, 7].my_map(proc { |item| item >= 4 }))
-
-puts
-puts 'Multiply_els'
-print multiply_els([1, 5, 3])
